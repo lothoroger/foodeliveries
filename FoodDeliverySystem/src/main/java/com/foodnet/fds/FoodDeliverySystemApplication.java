92a47048c4d5728c497090650df11ca1b3b5caff
Lotho <336536@win.lanl.gov>
2022-11-07 18:56:40 -0700

    Food Delivery Capstone project

---
+++ FoodDeliverySystem/src/main/java/com/foodnet/fds/FoodDeliverySystemApplication.java
@@ -0,0 +1,13 @@
+package com.foodnet.fds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliverySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliverySystemApplication.class, args);
+	}
+
+}
